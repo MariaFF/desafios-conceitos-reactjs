c04768c15163f67a829b86b52a383a421e12b463
MariaFF <mariadefatima775@hotmail.com>
2020-04-15 21:52:26 -0300

    Implementado adicionar/excluir repositorio

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+
+import api from './services/api'
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('/repositories', {
+      "title": `Novo repo ${Date.now()}`,
+      "techs": "['js', 'java']",
+      "url": "https://api.github.com/repos/octocat/Hello-World"
+    })
+    setRepositories([...repositories, response.data]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`/repositories/${id}`)
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+      {repositories.map(repo => (
+        <li key={repo.id}>         
+        {repo.title} 
+        <button onClick={() => handleRemoveRepository(repo.id)}>
+          Remover
+        </button>
+      </li>
+      ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
